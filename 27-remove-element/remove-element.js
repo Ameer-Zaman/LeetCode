b4bb8248337cb998eb5bd457f1d42f44267035ed
Ameer Zaman <150048864+Ameer-Zaman@users.noreply.github.com>
2026-08-31 10:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (48.68%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let dupArr = [];
+    let iterate = 0;
+
+    for(let i = 0;i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[iterate] = nums[i];
+            iterate++;
+        }
+    }
+
+    return iterate;
+};
+
+console.log(removeElement([3,2,2,3], 3))
